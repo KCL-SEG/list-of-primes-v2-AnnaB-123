e045d0aa62e1f5dd37e350862787ad5fa852a0f9
Anna Brown <anna.2.brown@kcl.ac.uk>
2022-10-06 00:04:42 +0100

    Add valueError exception to primes method

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    #checks if input is positive
+    if(number_of_primes <= 0):
+        raise ValueError ("Input should be positive")
+
     list = []
+    count = 2
+    div = 2
+
+    while (len(list) < number_of_primes):
+
+        if (count % div == 0):
+            #checks if itself
+            if (count == div):
+                list.append(count)
+            count += 1
+            div = 2
+        else:
+            div += 1
     return list
